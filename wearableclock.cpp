113e9aecf8b13348d883fa5fcc2b0b245daa126b
minji0801 <minji_0801@naver.com>
2020-07-10 10:01:15 +0900

    timer 제어만 Q_PROPERTY로 업데이트 (시간표기법 부분은 진행중)

--- wearableclock.cpp
+++ wearableclock.cpp
@@ -2,7 +2,10 @@
 
 WearableClock::WearableClock()
 {
-    show = true;
+    show = true;                // 시간 표기법과 관련된 변수
+    startOrStop = true;         // timer 시작할건지 끌건지
+    nowTimeText = "loading..."; // 현재 시간 초기화
+    nowAmPm = "";               // 오전오후 초기화
 }
 
 void WearableClock::setWindow(QQuickWindow *Window)
@@ -13,13 +16,52 @@ void WearableClock::setWindow(QQuickWindow *Window)
 
 void WearableClock::siganlAndSlot()
 {
-    QObject::connect(mainQml, SIGNAL(startTimerSignal()), this, SLOT(startTimer())); // qml에서 보낸 signal로 인해 startTimer () slot 함수 실행
-    QObject::connect(mainQml, SIGNAL(stopTimerSignal()), this, SLOT(stopTimer()));  // qml에서 보낸 signal로 인해 stopTimer() slot 함수 실행
+    //QObject::connect(mainQml, SIGNAL(startTimerSignal()), this, SLOT(startTimer())); // qml에서 보낸 signal로 인해 startTimer () slot 함수 실행
+    //QObject::connect(mainQml, SIGNAL(stopTimerSignal()), this, SLOT(stopTimer()));  // qml에서 보낸 signal로 인해 stopTimer() slot 함수 실행
     QObject::connect(this, SIGNAL(nowTimeToQml(QVariant)), mainQml, SLOT(timeSetLabel(QVariant)));    // 현재 시간을 담아서 cpp에서 qml로 signal을 보내어 qml에 있는 timeSetLabel() slot 함수 실행
 
     QObject::connect(mainQml, SIGNAL(howShowTimeSignal(bool)), this, SLOT(showChange(bool)));
     QObject::connect(this, SIGNAL(amOrPm(QVariant)), mainQml, SLOT(amPmSetLabel(QVariant)));
 }
+
+
+bool WearableClock::getbool() // startOrstop 값을 읽어오는 Q_PROPERTY READ 함수
+{
+    cout << "WearableClock::getbool value : " << startOrStop << endl;
+    return startOrStop;
+}
+
+void WearableClock::setbool(bool value) //  값을 수정하는 Q_PROPERTY WRITE 함수
+{
+    cout << "WearableClock::setbool value : " << value << endl;
+    startOrStop = value;
+    emit boolchanged(value);
+
+    if(startOrStop == true)
+    {
+        startTimer();
+    }
+
+    else if (startOrStop == false)
+    {
+        stopTimer();
+    }
+}
+
+
+bool WearableClock::getshow() // show 값을 읽어오는 Q_PROPERTY READ 함수
+{
+    cout << "WearableClock::getshow value : " << show << endl;
+    return show;
+}
+
+void WearableClock::setshow(bool value) //  show 값을 수정하는 Q_PROPERTY WRITE 함수
+{
+    cout << "WearableClock::setshow value : " << value << endl;
+    show = value;
+    emit showchanged(value);
+}
+
 
 void WearableClock::startTimer()    // QTimer 시작하는 함수
 {
@@ -40,31 +82,77 @@ void WearableClock::whatTimeNow()   // 현재 시간을 담아 보내는 함수
     cout << "whatTimeNow()!!" << endl;
     time = QTime::currentTime();    // 현재 시간을 time 변수에 담기
 
-    if (show == false) {
-        // am pm으로 보여주기
+    if (show == false)  // am pm으로 보여주기
+    {
         QString hourText = time.toString("hh"); // 오전 오후로 나누기 위해서 시만 hourText에 담기
 
-        if (hourText.toInt() >= 12) {
+        if (hourText.toInt() > 12)
+        {
             // 오후인 경우
             int hourInt = hourText.toInt() - 12;
             nowTimeText = QString::number(hourInt) + ":" + time.toString("mm:ss");
+            //cout << "nowTimeText value : " << nowTimeText.toUtf8().data() << endl;
+
             emit amOrPm("오후");
             emit nowTimeToQml(nowTimeText);
-        } else if (hourText.toInt() < 12) {
+
+            //nowAmPm = "오후";
+            //cout << "nowAmPm value : " << nowAmPm.toUtf8().data() << endl;
+        }
+
+        else if (hourText.toInt() == 12)
+        {
+            // 오후 12시 인 경우
+            nowTimeText = time.toString("h:mm:ss");
+            //cout << "nowTimeText value : " << nowTimeText.toUtf8().data() << endl;
+
+            emit amOrPm("오후");
+            emit nowTimeToQml(nowTimeText);
+
+            //nowAmPm = "오후";
+            //cout << "nowAmPm value : " << nowAmPm.toUtf8().data() << endl;
+        }
+
+        else if (hourText.toInt() < 12)
+        {
             // 오전인 경우
             nowTimeText = time.toString("h:mm:ss");
+            //cout << "nowTimeText value : " << nowTimeText.toUtf8().data() << endl;
+
             emit amOrPm("오전");
             emit nowTimeToQml(nowTimeText);
+
+            //nowAmPm = "오전";
+            //cout << "nowAmPm value : " << nowAmPm.toUtf8().data() << endl;
         }
-    } else if (show == true) {
-        // 24시간으로 보여주기
+    }
+
+    else if (show == true)  // 24시간으로 보여주기
+    {
         nowTimeText = time.toString("hh:mm:ss");
+        //cout << "nowTimeText value : " << nowTimeText.toUtf8().data() << endl;
+
         emit amOrPm("");
         emit nowTimeToQml(nowTimeText);
+
+        //nowAmPm = "";
+        //cout << "nowAmPm value : " << nowAmPm.toUtf8().data() << endl;
     }
 }
+
 
 void WearableClock::showChange(bool clickData)
 {
     show = clickData;
+}
+
+
+QString WearableClock::gettime()    // 현재 시간을 읽어오는 Q_PROPERTY READ 함수
+{
+    return nowTimeText;
+}
+
+QString WearableClock::getampm()    // 시간 표기법을 읽어오는 Q_PROPERTY READ 함수
+{
+    return nowAmPm;
 }
--- wearableclock.h
+++ wearableclock.h
@@ -1,6 +1,8 @@
 #ifndef WEARABLECLOCK_H
 #define WEARABLECLOCK_H
 
+#include "mainview.h"
+
 #include <QQuickView>
 #include <QObject>
 #include <QTimer>
@@ -8,12 +10,18 @@
 #include <iostream>
 #include <string>
 #include <QString>
+
 using namespace std;
 
 class WearableClock : public QObject
 {
 public:
     Q_OBJECT
+    Q_PROPERTY(bool p_bool READ getbool WRITE setbool NOTIFY boolchanged)   // timer를 시작할지 끝낼지 결정해주는 변수
+    Q_PROPERTY(bool p_show READ getshow WRITE setshow NOTIFY showchanged)
+
+    Q_PROPERTY(QString p_nowtime READ gettime NOTIFY timechanged)           // 현재 시간을 담고 있는 변수
+    Q_PROPERTY(QString p_ampm READ getampm NOTIFY ampmchanged)              // 시간 표기법을 담고 있는 변수
 
 public:
     WearableClock();
@@ -21,27 +29,46 @@ public:
     void setWindow(QQuickWindow *Window);
     void siganlAndSlot();
 
+    bool getbool();             // startOrStop 값을 읽어오는 Q_PROPERTY READ 함수
+    void setbool(bool value);   // startOrStop 값을 수정하는 Q_PROPERTY WRITE 함수
+
+    bool getshow();             // show 값을 읽어오는 Q_PROPERTY READ 함수
+    void setshow(bool value);   // show 값을 수정하는 Q_PROPERTY WRITE 함수
+
+    QString gettime();          // 현재 시간을 읽어오는 Q_PROPERTY READ 함수
+
+    QString getampm();          // 시간 표기법을 읽어오는 Q_PROPERTY READ 함수
+
 signals:
-    void startChangeComplete(); // start의 값을 변경하고 나면 실행되는 signal
+    //void startChangeComplete(); // start의 값을 변경하고 나면 실행되는 signal
     void nowTimeToQml(QVariant); // 현재 시간을 qml로 넘겨주는 signal
     void amOrPm(QVariant);  // am pm을 넘겨주는 signal
 
+    void boolchanged(const bool value); // startOrStop 값이 변경되었을 때 실행되는 Q_PROPERTY signal
+    void showchanged(const bool value); // show 값이 변경되었을 때 실행되는 Q_PROPERTY signal
+    void timechanged();                 // 현재 시간이 변경되었을 때 실행되는 Q_PROPERTY signal
+    void ampmchanged();                 // 시간 표기법이 변경되었을 때 실행되는 Q_PROPERTY signal
+
 public slots:
-    void startTimer();  // Timer를 시작하는 함수
-    void stopTimer();  // Timer를 없애는 함수
+    void startTimer();  // QTimer 객체를 만들어서 timer를 시작하는 함수
+    void stopTimer();   // QTimer를 객체를 없애는 함수
     void whatTimeNow(); // 현재 시간을 구해주는 함수
 
-    void showChange(bool);  // 시간 표기법 관련 함수
+    void showChange(const bool value);  // 시간 표기법 관련 함수
 
 private:
     QQuickWindow *mainQml;
 
-    bool start; // qml에서 startTimerSignal을 받았을 때 true 값으로 바꿔주어 timer를 실행하기 위한
+    //bool start;             // qml에서 startTimerSignal을 받았을 때 true 값으로 바꿔주어 timer를 실행하기 위한
     QTimer *timer;
     QTime time;
-    QString nowTimeText;
+    QString nowTimeText;    // 현재 시간을 담고 있는 변수
 
-    bool show;  // 시간 표기법 관련 변수
+    bool show;              // 시간 표기법 관련 변수
+
+    bool startOrStop;       // timer를 start 할건지 stop 할 건지 구분해주는 변수
+
+    QString nowAmPm;        // 오전/오후/"" 값을 담고 있는 변수
 };
 
 #endif // WEARABLECLOCK_H
